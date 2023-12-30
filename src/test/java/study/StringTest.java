6c96d3b8b21449f73c8dd6528ecaf15474b0af9e
Lee seung chan <dltmdcks702@gmail.com>
2023-12-30 22:27:31 +0900

    feat: init split test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
@@ -9,5 +11,18 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String value = "1,2";
+        String[] splitValue = value.split(",");
+
+        assertThat(splitValue).contains("1", "2");
+
+        value = "1";
+        splitValue = value.split(",");
+
+        assertThat(splitValue).containsExactly("1");
     }
 }
